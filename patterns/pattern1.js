6c89819bce69e27ead322e93427909ff68ae16c9
Aneesh165 <aneeshmp165@gmail.com>
2024-06-18 11:27:25 +0530

    18-06-2024

---
+++ patterns/pattern1.js
@@ -0,0 +1,10 @@
+function pattern(n){
+    for(let i=1; i<=n; i++){
+        let arra=" ";
+        for(let i=1; i<=n; i++){
+            arra +="*"+" ";
+        }
+        console.log(arra);
+    }
+}
+pattern(4);
