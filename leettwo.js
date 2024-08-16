fd7f519f727f91d1fee9bf83f02bc0a3dd9d325a
Aneesh165 <aneeshmp165@gmail.com>
2024-08-16 09:43:01 +0530

    15-08-2024

--- leettwo.js
+++ leettwo.js
@@ -1,7 +1,27 @@
-function two(){
-    let nums=[2,2,1];
-    if(){}
 
+    let nums = [-1,-1,2,-4,2];
+    let result = 0;
+    for (let num of nums) {
+        result ^= num;
+    }
+    console.log(result);
+     
+    // let n=nums.length;
+    // if(n==1){
+    //     console.log(nums[0])
+    // }
+    // if(n==3){
+    //     let num=nums.sort((a,b)=>a-b)
+    //     let last=num.reduce((a,b)=>a.num!=b.num?b:a)
+    //     console.log(last);
 
-}
-two()
+    // }
+    
+    // if(n>3){
+    //     let sorted=nums.sort((a,b)=>a-b)
+    //     let output=sorted.reduce((a,b)=>a.sorted!=b.sorted?a:b)
+    //     console.log(sorted);
+    //     console.log(output);
+
+    // }       
+
